7cafbbfd65fd2bedc36a69dd27ef42598ed163ba
won-joon <seowj819@gmail.com>
2020-10-19 14:59:27 +0900

    뉴스 구현

--- routes/group.js
+++ routes/group.js
@@ -31,10 +31,50 @@ router.get('/join', (req, res) => {
 });
 
 router.get('/manage', (req, res) => {
+    const NewsAPI = require('newsapi');
+    const newsapi = new NewsAPI('98fbe85e58d84aa3afd2fb1b56ec4ccb');
+    // To query /v2/top-headlines
+    // All options passed to topHeadlines are optional, but you need to include at least one of them
+    newsapi.v2.everything({
+        q: 'disease',
+        from: '2020-10-01',
+        to: '2020-10-16',
+        language: 'en',
+        sortBy: 'relevancy',
+        page: 2
+    }).then(response => {
+        res.send(response);
+        /*
+            {
+            status: "ok",
+            articles: [...]
+            }
+        */
+        });
+
+    /*
+    var client_id = '3km6JC1yYh_bucjn9tjx';
+    var client_secret = 'sKXTDPbesY';
+    var api_url = 'https://openapi.naver.com/v1/search/news?query='+'%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=sim' encodeURI(req.query.query); // json 결과
+    var request = require('request');
+    var options = {
+        url: api_url,
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+    request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+    
     res.render('group_manage', {
         user: req.user,
         loginError: req.flash('loginError'),
-    });
+    });*/
 });
 
 router.post('/create', async (req, res, next) => {
